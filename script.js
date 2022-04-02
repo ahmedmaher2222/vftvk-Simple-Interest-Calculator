1fc6e77473391c62b5de590eb963f21cb1e305f4
ahmedmaher2222 <beboga559@gmail.com>
2022-04-02 22:41:34 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function updateRate(){
+    var interest = document.getElementById("rate").value;
+    document.getElementById("rate_display").innerHTML = interest + '%';
 }
-        
+function compute() 
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    
+    var result = principal * years * rate /100;
+    if (principal <=0){
+        window.alert("enter a positive number");
+    }
+
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("actualYear").innerHTML = year;
+    document.getElementById("result").innerText=result;
+}
